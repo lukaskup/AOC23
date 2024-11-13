7b3557fa9b9a0027a91437e7001496d988c05c33
lukaskup <lukasz.skup1999@gmail.com>
2024-11-13 22:14:11 +0100

    end of work commit

--- day1/main.zig
+++ day1/main.zig
@@ -3,22 +3,20 @@ const std = @import("std");
 const print = std.debug.print;
 
 pub fn main() !void {
-    try readFile();
+    try sumCalibrationValues();
 }
-fn readFile() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    const data = std.ArrayList(u8).init(allocator);
+fn sumCalibrationValues() !void {
     const file = try std.fs.cwd().openFile("input.txt", .{});
-
     defer file.close();
-    defer data.deinit();
 
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
 
     var buf: [1024]u8 = undefined;
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        print("{s}\n", .{line});
+        for (line) |v| {
+            //TODO get only numbers
+            print("{c}\n", .{v});
+        }
     }
 }
