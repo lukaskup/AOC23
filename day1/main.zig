b27dc015c19261dada315a9200ef2f76cf6332d9
lukaskup <lukasz.skup1999@gmail.com>
2024-11-14 19:36:16 +0100

    part 2 of day 1
    Co-authored-by: qbiwoj <qbiwoj@users.noreply.github.com>

--- day1/main.zig
+++ day1/main.zig
@@ -3,9 +3,11 @@ const std = @import("std");
 const print = std.debug.print;
 
 pub fn main() !void {
-    try sumCalibrationValues();
+    const calibrationValue = try getCalibrationValue();
+    print("calibration value {}\n", .{calibrationValue});
 }
-fn sumCalibrationValues() !void {
+
+fn getCalibrationValue() !u32 {
     const file = try std.fs.cwd().openFile("input.txt", .{});
     defer file.close();
 
@@ -13,10 +15,67 @@ fn sumCalibrationValues() !void {
     var in_stream = buf_reader.reader();
 
     var buf: [1024]u8 = undefined;
+    var sum: u32 = 0;
+
+    var map = std.StringHashMap(*const [1:0]u8).init(std.heap.page_allocator);
+    try map.put("one", "1");
+    try map.put("two", "2");
+    try map.put("three", "3");
+    try map.put("four", "4");
+    try map.put("five", "5");
+    try map.put("six", "6");
+    try map.put("seven", "7");
+    try map.put("eight", "8");
+    try map.put("nine", "9");
+
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        for (line) |v| {
-            //TODO get only numbers
-            print("{c}\n", .{v});
+        var digits = std.ArrayList(u8).init(std.heap.page_allocator);
+        defer digits.deinit();
+        // 9eight3 - example input value
+
+        for (line) |c| {
+            if (c >= '0' and c <= '9') {
+                digits.append(c) catch |e| {
+                    print("{}", .{e});
+                };
+            }
         }
+        const first_digit = digits.items[0] - '0';
+        const last_digit = digits.items[digits.items.len - 1] - '0';
+
+        sum += first_digit * 10 + last_digit;
     }
+    // 37060
+    return sum;
+}
+
+fn replace_string(str: []const u8, map: std.StringHashMap(*const [1:0]u8)) void {
+    _ = str; // autofix
+    // iterate over str
+    // save char to buffer
+    // check if buffer exists in map keys
+    //lh5jzqxeight84
+    var iterator = map.keyIterator();
+    while (iterator.next()) |key| {
+        const val = map.get(key.*).?[0] - '0';
+        print("{s} -> {}\n", .{ key.*, val });
+    }
+
+    // [][]const u8
+    // _ = str; // autofix
+}
+
+test {
+    var map = std.StringHashMap(*const [1:0]u8).init(std.heap.page_allocator);
+    try map.put("one", "1");
+    try map.put("two", "2");
+    try map.put("three", "3");
+    try map.put("four", "4");
+    try map.put("five", "5");
+    try map.put("six", "6");
+    try map.put("seven", "7");
+    try map.put("eight", "8");
+    try map.put("nine", "9");
+
+    replace_string("9eight3", map);
 }
